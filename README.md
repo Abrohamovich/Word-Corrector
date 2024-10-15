cfa1996802ca5f6d94ebfd3cc3dfcea32048d27b
Mykyta Herasymov <gerasimov08nikita@gmail.com>
2024-10-15 23:56:59 +0300

    Bug fixing

---
+++ README.md
@@ -0,0 +1,9 @@
+# Java Project Documentation
+
+---
+
+This Java project contains three classes: `Main`, `DataProvider`, and `Corrector`.
+The programme accepts an array of strings with words where some characters have been replaced by the ‘b’ character.
+Using `Correct` class replaces "b" with "o" (if they are) in all strings of the array.
+
+
--- src/main/java/app/Corrector.java
+++ src/main/java/app/Corrector.java
@@ -3,14 +3,14 @@ package app;
 public class Corrector {
 
     public String handleData(String[] strs) {
-        stringBuilder = new StringBuilder();
+        StringBuilder stringBuilder = new StringBuilder();
         int count = 0;
         for (String str : strs) {
-            if (str.("b")) {
-                str = str.replac("b", "o");
+            if (str.contains("b")) {
+                str = str.replace("b", "o");
             }
-            count;
-            stringBuilder.append(count).append(") ").append(str).append("\\n");
+            count++;
+            stringBuilder.append(count).append(") ").append(str).append("\n");
         }
         return stringBuilder.toString();
     }
